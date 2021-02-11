327b3c426828b466979581cc46dd4286726ce4f2
0xRar <33517160+0xRar@users.noreply.github.com>
2021-02-11 10:33:54 +0300

    Create EmployeeMain.java

---
+++ warmup/Employee1/EmployeeMain.java
@@ -0,0 +1,25 @@
+public class EmployeeMain {
+    public static void main(String[] args) {
+        Employee Name = new Employee();
+        Employee idNumber = new Employee();
+        Employee Department = new Employee();
+        Employee Position = new Employee();
+
+
+        Name.setName("Susan Mayers");
+        idNumber.setIdNumber(47899);
+        Department.setDepartment("Accounting");
+        Position.setPosition("Vice President");
+
+        Name.setName("Mark Jones");
+        idNumber.setIdNumber(39119);
+        Department.setDepartment("IT");
+        Position.setPosition("Programmer");
+
+        Name.setName("Joy Rogers");
+        idNumber.setIdNumber(81774);
+        Department.setDepartment("Manufacturing");
+        Position.setPosition("Engineer");
+    }
+    // Isa Ebrahim (0xRar)
+}
