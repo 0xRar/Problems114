7064e55468caca64a3e51b68051085e2c140412d
0xRar <33517160+0xRar@users.noreply.github.com>
2021-03-08 17:43:36 +0300

    Arrays

---
+++ ArrayPractice1/ArrayPractice1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ArrayPractice1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int Arr[] = new int[5];
+
+        for (int i=0; i < Arr.length; i++) {
+
+            System.out.print(i +" Enter an integer number 5 times: ");
+            Arr[i] = input.nextInt();
+        }
+
+        for (int i=0; i < Arr.length; i++) {
+
+            if (Arr[i] % 2 == 1) {
+                System.out.println("The First odd number is: " + Arr[i]);
+            }
+            return;
+        }
+        // Isa Ebrahim (0xRar)
+    }
+}
